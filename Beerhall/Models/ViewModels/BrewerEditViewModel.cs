6b870a08330fc8a9b2c0f00c04b5f4cd8ba60708
Stefaan Samyn <stefaan.samyn@hogent.be>
2017-12-07 11:33:24 +0100

    Add client side validation

--- Beerhall/Models/ViewModels/BrewerEditViewModel.cs
+++ Beerhall/Models/ViewModels/BrewerEditViewModel.cs
@@ -6,16 +6,20 @@ namespace Beerhall.Models.ViewModels
 {
     public class BrewerEditViewModel
     {
+        [Required]
+        [StringLength(50, ErrorMessage = "{0} may not contain more than 50 characters")]
         public string Name { get; set; }
         [Display(Prompt = "Street and house number")]
         public string Street { get; set; }
         [Display(Name = "Location")]
         public string PostalCode { get; set; }
         [DataType(DataType.Currency)]
+        [Range(0, int.MaxValue, ErrorMessage = "{0} may not be a negative value.")]
         public int? Turnover { get; set; }
         public string Description { get; set; }
         [Display(Name = "Email address")]
         [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Email address is not valid")]
         public string ContactEmail { get; set; }
         [Display(Name = "Date established")]
         [DataType(DataType.Date)]
